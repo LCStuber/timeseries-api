8a125191feee1fa1fef67c4760040d4e83a9483b
SocioPJ <sociopj45@outlook.com>
2024-04-01 15:40:51 -0300

    Threshold of interval

--- controller/smartlight_controller.go
+++ controller/smartlight_controller.go
@@ -3,44 +3,26 @@ package controller
 import (
 	"context"
 	"net/http"
-
+	"fmt"
 	"github.com/OpenDataTelemetry/timeseries-api/database"
 	"github.com/gin-gonic/gin"
+	"strconv"
 )
 
-type SmartLight struct {
-	Fields struct {
-			DataCounter0d0               float64 `json:"data_counter_0d_0"`
-			DataCounter0d1               float64 `json:"data_counter_0d_1"`
-			DataEnergy0                  float64 `json:"data_energy_0"`
-			DataEnergy1                  float64 `json:"data_energy_1"`
-			FCnt                         int     `json:"fCnt"`
-			RxInfoAltitude0              float64 `json:"rxInfo_altitude_0"`
-			RxInfoLatitude0              float64 `json:"rxInfo_latitude_0"`
-			RxInfoLoRaSNR0               int     `json:"rxInfo_loRaSNR_0"`
-			RxInfoLongitude0             float64 `json:"rxInfo_longitude_0"`
-			RxInfoRssi0                  int     `json:"rxInfo_rssi_0"`
-			TxInfoDataRateSpreadFactor   int     `json:"txInfo_dataRate_spreadFactor"`
-			TxInfoFrequency              int     `json:"txInfo_frequency"`
-	} `json:"fields"`
-	Name string `json:"name"`
-	Tags struct {
-			ApplicationID            string  `json:"applicationID"`
-			DevEUI                   string  `json:"devEUI"`
-			FPort                    string  `json:"fPort"`
-			NodeName                 string  `json:"nodeName"`
-			RxInfoMac0               string  `json:"rxInfo_mac_0"`
-			RxInfoName0              string  `json:"rxInfo_name_0"`
-			TxInfoAdr                string  `json:"txInfo_adr"`
-			TxInfoCodeRate           string  `json:"txInfo_codeRate"`
-			TxInfoDataRateBandwidth  string  `json:"txInfo_dataRate_bandwidth"`
-			TxInfoDataRateModulation string  `json:"txInfo_dataRate_modulation"`
-	} `json:"tags"`
-	Timestamp int64 `json:"timestamp"`
-}
 
 func GetSmartLights(c *gin.Context) {
-	interval := c.Query("interval")
+	intervalStr := c.Query("interval")
+	interval, err := strconv.Atoi(intervalStr)
+
+	if err != nil {
+    c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid interval value"})
+    return
+	}
+
+	if interval > 400{
+		c.JSON(400, gin.H{"error": "Interval must be less than 1000"})
+		return
+	}
 
 	var objs = []gin.H{}
 	influxDB, err := database.ConnectToDB()
@@ -54,7 +36,7 @@ func GetSmartLights(c *gin.Context) {
 	query := `
 		SELECT *
 		FROM "SmartLights"
-		WHERE "time" >= now() - interval '` + interval + ` minutes'
+		WHERE "time" >= now() - interval '` + intervalStr + ` minutes'
 		ORDER BY time DESC;
 	`
 
@@ -85,7 +67,7 @@ func GetSmartLights(c *gin.Context) {
 			},
 			"name": "SmartLights",
 			"tags": gin.H{
-					// Substitua as chaves e valores abaixo pelos valores apropriados
+					
 					"applicationID": value["applicationID"],
 					"devEUI": value["devEUI"],
 					"fPort": value["fPort"],
@@ -97,14 +79,13 @@ func GetSmartLights(c *gin.Context) {
 					"txInfo_dataRate_bandwidth": "125",
 					"txInfo_dataRate_modulation": "LORA",
 			},
-			"timestamp": value["time"], // Substitua por value["timestamp"] se o timestamp estiver disponível no valor
+			"timestamp": value["time"], 
 	}       
-		// "data_counter_0d_0" : value["data_counter_0d_0"]}       // Convert the row to a gin.H map (JSON)
+		// Convert the row to a gin.H map (JSON)
 		objs = append(objs, obj)  // Append the row to the objs slice
 	}
+	fmt.Println(len(objs))
 	c.IndentedJSON(http.StatusOK, objs)
-
-
 }
 
 func GetSmartLightbyNodeName(c *gin.Context) {
@@ -145,10 +126,12 @@ func GetSmartLightbyNodeName(c *gin.Context) {
 					"rxInfo_rssi_0": value["rxInfo_rssi_0"],
 					"txInfo_dataRate_spreadFactor": value["txInfo_dataRate_spreadFactor"],
 					"txInfo_frequency": value["txInfo_frequency"],
+					"data_humidity": value["data_humidity"],
+					"data_temperature": value["data_temperature"],
 			},
 			"name": "SmartLights",
 			"tags": gin.H{
-					// Substitua as chaves e valores abaixo pelos valores apropriados
+					
 					"applicationID": value["applicationID"],
 					"devEUI": value["devEUI"],
 					"fPort": value["fPort"],
@@ -160,7 +143,7 @@ func GetSmartLightbyNodeName(c *gin.Context) {
 					"txInfo_dataRate_bandwidth": "125",
 					"txInfo_dataRate_modulation": "LORA",
 			},
-			"timestamp": value["time"], // Substitua por value["timestamp"] se o timestamp estiver disponível no valor
+			"timestamp": value["time"], 
 	}       // Convert the row to a gin.H map (JSON)
 		objs = append(objs, obj)  // Append the row to the objs slice
 	}
@@ -195,8 +178,8 @@ func GetSmartLightbyDevEUI(c *gin.Context) {
 		panic(err)
 	}
 
-	for iterator.Next() { // Iterate over query response
-		value := iterator.Value() // Value of the current row
+	for iterator.Next() { 
+		value := iterator.Value() 
 		obj := gin.H{
 			"fields": gin.H{
 					"data_counter_0d_0": value["data_counter_0d_0"],
@@ -211,10 +194,11 @@ func GetSmartLightbyDevEUI(c *gin.Context) {
 					"rxInfo_rssi_0": value["rxInfo_rssi_0"],
 					"txInfo_dataRate_spreadFactor": value["txInfo_dataRate_spreadFactor"],
 					"txInfo_frequency": value["txInfo_frequency"],
+					"data_humidity": value["data_humidity"],
+					"data_temperature": value["data_temperature"],
 			},
 			"name": "SmartLights",
 			"tags": gin.H{
-					// Substitua as chaves e valores abaixo pelos valores apropriados
 					"applicationID": value["applicationID"],
 					"devEUI": value["devEUI"],
 					"fPort": value["fPort"],
@@ -226,13 +210,13 @@ func GetSmartLightbyDevEUI(c *gin.Context) {
 					"txInfo_dataRate_bandwidth": "125",
 					"txInfo_dataRate_modulation": "LORA",
 			},
-			"timestamp": value["time"], // Substitua por value["timestamp"] se o timestamp estiver disponível no valor
-	}       // Convert the row to a gin.H map (JSON)
-		objs = append(objs, obj)  // Append the row to the objs slice
+			"timestamp": value["time"], 
+	}       
+		objs = append(objs, obj)  
 	}
 	if len(objs) == 0 {
 		c.JSON(http.StatusNotFound, gin.H{"error": "deviceId not found!"})
 		return
 	}
-	c.IndentedJSON(http.StatusOK, objs) // Return the objs slice as a JSON response
+	c.IndentedJSON(http.StatusOK, objs) 
 }
